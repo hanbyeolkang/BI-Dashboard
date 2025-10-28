e2bf4cf6e521eb3a4b2b1a25f9257c47df6b7975
hanbyeolkang <strongstar1210@gmail.com>
2025-10-28 11:40:59 +0900

    관객수 차트 요일 변경 (매출액과 같게)

--- scripts/daily/chart_daily_audience.sql
+++ scripts/daily/chart_daily_audience.sql
@@ -2,10 +2,11 @@
 
 SELECT
   EXTRACT(DAY FROM showrange) AS "날짜",
-  dayofweek_order AS dayofweek_order,
-  dayofweek_ko AS dayofweek_ko,
-  max(totalaudicnt) AS "관객수" 
+  CAST(dayofweek_order AS VARCHAR) || '.' || dayofweek_ko AS "요일",
+  MAX(totalaudicnt) AS "관객수"
 FROM adhoc.daily_sum
-GROUP BY EXTRACT(DAY FROM showrange), dayofweek_order, dayofweek_ko
+GROUP BY
+  EXTRACT(DAY FROM showrange),
+  CAST(dayofweek_order AS VARCHAR) || '.' || dayofweek_ko
 ORDER BY "관객수" DESC
-LIMIT 10000;
+LIMIT 10000
