9c132c4661903fc2cfe8df6ed119068f8fb3d900
427paul <161034456+427paul@users.noreply.github.com>
2025-10-28 14:52:41 +0900

    Add files via upload

---
+++ scripts/time_series_audience_script/daily_avg_audience_ratio.sql
@@ -0,0 +1,23 @@
+create table analytics.daily_avg_audience_ratio as
+WITH daily_data AS (
+    SELECT
+        b.movieCd,
+        m.movieNm,
+        DATEDIFF('day', m.openDt, b.showRange) AS days_after_release,
+        SUM(b.audiCnt) AS day_audience,
+        MAX(b.audiAcc) AS total_audience
+    FROM raw_data.boxoffice b
+    JOIN raw_data.movie_info m
+        ON b.movieCd = m.movieCd
+    GROUP BY b.movieCd, m.movieNm, DATEDIFF('day', m.openDt, b.showRange)
+)
+SELECT
+    days_after_release,
+    COUNT(movieCd) AS movie_count,
+    SUM(day_audience) AS sum_day_audience,
+    SUM(total_audience) AS sum_total_audience,
+    (SUM(day_audience)::FLOAT / SUM(total_audience)) AS day_ratio
+FROM daily_data
+WHERE days_after_release BETWEEN 0 AND 13
+GROUP BY days_after_release
+ORDER BY days_after_release;
