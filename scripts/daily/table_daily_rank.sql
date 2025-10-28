27673ba83218d3e2bcd40766f2fea43c99989719
hanbyeolkang <strongstar1210@gmail.com>
2025-10-28 15:47:46 +0900

    영화별 랭킹 차트 추가

--- scripts/daily/table_daily_rank.sql
+++ scripts/daily/table_daily_rank.sql
@@ -1,12 +1,31 @@
--- 일일 영화별 랭킹 테이블
+-- 일일 영화별 랭킹 테이블 (9월 관객수 기준 상위10 영화 한정)
 
 CREATE TABLE adhoc.daily_rank AS
-SELECT 
-    b.showrange,
-    b.rank,
-    11 - b.rank AS reverse_rank,
-    m.movieCd,
-    m.movieNm
+SELECT
+  b.showrange,
+  b.rank,
+  (11 - b.rank) AS reverse_rank,
+  b.movieCd,
+  m.movieNm
 FROM raw_data.boxoffice AS b
-LEFT JOIN raw_data.movie_Info AS m ON b.movieCd = m.movieCd
-ORDER BY b.showrange, b.rank;
+JOIN raw_data.movie_info AS m ON b.movieCd = m.movieCd
+JOIN (
+  SELECT
+    movieCd
+    ,SUM(audiCnt) as sumAudiCnt  -- 9월 관객수 합
+  FROM raw_data.boxoffice
+  GROUP BY movieCd
+  ORDER BY sumAudiCnt DESC
+  LIMIT 10
+) AS t ON b.movieCd = t.movieCd
+
+
+ALTER TABLE adhoc.daily_rank    
+ADD COLUMN audiCnt INT;
+
+
+UPDATE adhoc.daily_rank AS r
+SET audicnt = b.audicnt
+FROM raw_data.boxoffice AS b
+WHERE r.showrange = b.showrange
+  AND r.rank = b.rank;
